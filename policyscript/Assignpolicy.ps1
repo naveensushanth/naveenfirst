a06090761d30195fb341dcf31bc0199346e30155
naveensushanth <45732282+naveensushanth@users.noreply.github.com>
2021-06-10 10:50:16 +0530

    Create Assignpolicy.ps1

---
+++ policyscript/Assignpolicy.ps1
@@ -0,0 +1,30 @@
+$policyObjs = ConvertFrom-Json -InputObject $env:POLICYDEFS
+Param(
+[String]$policyDefRootFolder,
+[Parameter(Mandatory = $false)][String]$Subscriptionname,
+[Parameter(Mandatory = $false)][String]$policyAssignmentRG 
+)
+if($null -ne $policyAssignmentRG){
+$resourcegroupID = ((Get-AzResourceGroup -Name $policyAssignmentRG).ResourceId)
+}
+
+if($null -ne $Subscriptionname){
+$SubscriptionId = ((Get-AzSubscription -SubscriptionName $Subscriptionname).Id)
+}
+
+foreach ($policyDefFolder in (Get-ChildItem -Path $policyDefRootFolder -Directory)) {
+
+    Write-Host Processing folder: $policyDefFolder.Name
+    $selected = $policyObjs | Where-Object { $_.Name -eq $policyDefFolder.Name }
+    Write-Host Creating assignment for: $selectedObj
+   if($null -ne $resourcegroupID)
+   {
+    New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $selected -Scope $resourcegroupID -PolicyParameter  "$($policyDefFolder.FullName)\values.$(Release.EnvironmentName).json"
+
+   }
+   else{
+     New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $selected -Scope "/subscriptions/$SubscriptionId"  
+   
+   }
+   
+}
