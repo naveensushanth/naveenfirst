47c2b94c1e6284f6fbb109fdb1b20a61e56b579b
naveensushanth <45732282+naveensushanth@users.noreply.github.com>
2021-06-12 21:29:37 +0530

    Update Assignpolicy.ps1

--- policyscript/Assignpolicy.ps1
+++ policyscript/Assignpolicy.ps1
@@ -1,29 +1,10 @@
 Param(
 [String]$policyDefRootFolder,
-[Parameter(Mandatory = $false)][String]$Subscriptionname,
-[Parameter(Mandatory = $false)][String]$policyAssignmentRG = ''
+[Parameter(Mandatory = $false)][String]$policyAssignmentRG 
 )
 $policyObjs = ConvertFrom-Json -InputObject $env:POLICYDEFS
 write-host "set parameters '$($policyDefRootFolder)' & '$($env:POLICYDEFS)'"
 write-host "set parameters '$($policyAssignmentRG)'"
-
-if($null -eq $policyAssignmentRG ){
-write-host "This assignment is for Subscription level"
-
-}else
-{
-$resourcegroupID = ((Get-AzResourceGroup -Name $policyAssignmentRG).ResourceId)
-}
-
-if($null -eq $Subscriptionname){
-write-host "This assignment is for Resource group level"
-
-}
-else
-{
-write-host "wrong loop"
-$SubscriptionId = ((Get-AzSubscription -SubscriptionName $Subscriptionname).Id)
-}
 
 foreach ($policyDefFolder in (Get-ChildItem -Path $policyDefRootFolder -Directory)) {
 
@@ -31,14 +12,8 @@ foreach ($policyDefFolder in (Get-ChildItem -Path $policyDefRootFolder -Director
     $selected = $policyObjs | Where-Object { $_.Name -eq $policyDefFolder.Name }
     
     Write-Host "Creating assignment for: '$($selected)'"
-   if($null -ne $resourcegroupID)
-   {
-    New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $selected -Scope $resourcegroupID -PolicyParameter  "$($policyDefFolder.FullName)\values.$(Release.EnvironmentName).json"
-
-   }
-   else{
-     New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $selected -Scope "/subscriptions/$SubscriptionId"  
    
-   }
+    New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $selected -Scope ((Get-AzResourceGroup -Name $policyAssignmentRG).ResourceId) -PolicyParameter  "$($policyDefFolder.FullName)\values.$(Release.EnvironmentName).json"
+
    
 }
