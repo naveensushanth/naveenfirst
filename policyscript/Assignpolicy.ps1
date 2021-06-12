ea096e048eee555c20b9a150081ce57074929d6e
naveensushanth <45732282+naveensushanth@users.noreply.github.com>
2021-06-12 22:41:13 +0530

    Update Assignpolicy.ps1

--- policyscript/Assignpolicy.ps1
+++ policyscript/Assignpolicy.ps1
@@ -5,7 +5,7 @@ $policyDefRootFolder = "_test-CI/drop"
 foreach ($policyDefFolder in (Get-ChildItem -Path $policyDefRootFolder -Directory)) {
 
     Write-Host Processing folder: $policyDefFolder.Name
-    $selected = $policyObjs | Where-Object { $_.Name -eq $policyDefFolder.Name }
+    $selected =  Get-AzPolicyDefinition -Name $policyDefFolder.Name
     Write-Host Creating assignment for: $selected
     write-host "select release environment '$($Release.EnvironmentName))'"
     New-AzPolicyAssignment -Name $policyDefFolder.Name -PolicyDefinition $selected -Scope ((Get-AzResourceGroup -Name $policyAssignmentRG).ResourceId) -PolicyParameter  "$($policyDefFolder.FullName)\values.dev.json"
